618702dd17ddbbdf279de115c6d0108a6f6ae51d
Pallab Bharman <bharmanpallab@gmail.com>
2023-06-06 09:19:29 +0600

    more error handler added

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,20 @@ import app from './app';
 import connectDB from './configs/db.config';
 import envConfig from './configs/env.config';
 
+process.on('uncaughtException', (error) => {
+    errorLogger.error(error);
+    process.exit(1);
+});
+
+let server: Server;
+
 const startServer = async () => {
     await connectDB();
-    const server: Server = app.listen(envConfig.port, () => {
+    server = app.listen(envConfig.port, () => {
         logger.info(`Server running on port ${envConfig.port || 5000}`);
     });
 
     process.on('unhandledRejection', (error) => {
-        console.log('Unhandled Rejection is detected, server is closing now!');
-
         if (server) {
             server.close(() => {
                 errorLogger.error(error);
@@ -25,3 +30,10 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM is received');
+    if (server) {
+        server.close();
+    }
+});
